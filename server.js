bfc4816662a93d5e274d35d2b0e1161ac8323042
zmiles17 <zgm_striker17@hotmail.com>
2018-11-12 14:10:33 -0500

    Basic layout

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/socketlist", { useNewUrlParser: true });
+
+require("./sockets/message-sockets")(io);
+require("./routes/api-routes")(app);
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`);
+})
