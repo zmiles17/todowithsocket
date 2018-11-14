31e5398873405252ae18a96632e85b9bd48fd96c
MoNsTeR <zgm_striker17@hotmail.com>
2018-11-13 21:17:30 -0500

    Update works need to work on delete route

--- public/app.js
+++ public/app.js
@@ -1,16 +1,30 @@
+const date = new Date();
+const day = ["SUN", "MON", "TUES", "WED", "THURS", "FRI", "SAT"];
+const month = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+$("header").append(`<h1>${day[date.getDay()]}</h1><h4>${month[date.getMonth()]}${date.getDate()}</h4><h4>${date.getFullYear()}</h4>`);
 
 function renderChecklist() {
     $.get("/checklist").then(function (res) {
         $("ul").empty();
-        res.forEach(e => $("ul").append(`<li data-id="${e._id}">${e.todo}<i icon-id="${e._id}" class="fal fa-circle"></i></li>`));
-        $(".fa-circle").on("click", function () {
-            const id = $(event.target).parent().attr("data-id");
-            $.ajax({ url: `/checklist/${id}`, method: "PUT", data: { completed: true } }).then(function (res) {
-                if(this.data === "completed=true"){
-                    console.log("Hi");
-                };
-            })
-        })
+        for (let i = 0; i < res.length; i++) {
+            if (res[i].completed === false) {
+                $("ul").empty();
+                res.forEach(e => $("ul").append(`<li data-id="${e._id}">${e.todo}<i class="far fa-circle"></i></li>`));
+                $("li").on("click", function (event) {
+                    if ($(event.target).attr("class") === "far fa-circle") {
+                        const id = $(event.target).parent().attr("data-id");
+                        $.ajax({ url: `/checklist/${id}`, method: "PUT", data: { completed: true } }).then(function (res) {
+                            $(event.target).removeClass("fal fa-circle").addClass("far fa-times-circle");
+                            console.log(res);
+                        })
+                    }
+                })
+            }
+            else if (res[i].completed === true) {
+                $("ul").empty();
+                res.forEach(e => $("ul").append(`<li data-id="${e._id}">${e.todo}<i class="far fa-times-circle"></i></li>`));
+            }
+        }
     })
 }
 
