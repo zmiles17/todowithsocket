a9d048905535f59afb614af6d210980cda509d39
zmiles17 <zgm_striker17@hotmail.com>
2018-11-14 21:35:52 -0500

    Heroku and deployed link not working

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended: true}));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static("public"));
+app.use(bodyParser.json());
 
-mongoose.Promise = Promise;  
+ 
 mongoose.connect('mongodb://localhost/checklist', { useNewUrlParser: true });
 
 require("./sockets/checklist-sockets")(io);
